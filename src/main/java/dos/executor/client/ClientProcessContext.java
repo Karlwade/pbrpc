a52d2d5121bf672bea54d596cc8cc96fdd8c2f8e
imotai <codego.me@gmail.com>
2016-08-12 19:23:20 +0800

    add distributed client

--- src/main/java/dos/executor/client/ClientProcessContext.java
+++ src/main/java/dos/executor/client/ClientProcessContext.java
@@ -1,7 +1,63 @@
 package dos.executor.client;
 
-public class ClientProcessContext {
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import dos.executor.ExecutorDescriptor.CallableTask;
+import dos.executor.ExecutorDescriptor.CallableTaskResult;
+
+public class ClientProcessContext implements Future<CallableTaskResult> {
 
     private Object tx = new Object();
+    private CallableTask task;
+    private CallableTaskResult result;
+    private boolean completed;
+    public CallableTask getTask() {
+        return task;
+    }
+    public void setTask(CallableTask task) {
+        this.task = task;
+    }
+    public CallableTaskResult getResult() {
+        return result;
+    }
+    public void setResult(CallableTaskResult result) {
+        this.result = result;
+    }
+    
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean isCancelled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean isDone() {
+        return completed;
+    }
+    
+    @Override
+    public CallableTaskResult get() throws InterruptedException, ExecutionException {
+        synchronized (tx) {
+            while (!completed) {
+                tx.wait(1000);
+            }
+        }
+        return result;
+    }
+    
+    
+    @Override
+    public CallableTaskResult get(long timeout, TimeUnit unit)
+            throws InterruptedException, ExecutionException, TimeoutException {
+        
+        return null;
+    }
     
 }
--- src/main/java/dos/executor/client/DistributedClient.java
+++ src/main/java/dos/executor/client/DistributedClient.java
@@ -1,16 +1,22 @@
 package dos.executor.client;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dos.executor.ExecutorDescriptor;
 import io.netty.channel.ChannelFuture;
+import io.protostuff.Schema;
 
 public class DistributedClient {
     private static final Logger logger = LoggerFactory.getLogger(DistributedClient.class);
     private volatile ChannelFuture channel = null;
-    
-    public void sendTask(ExecutorDescriptor.CallableTask task) {
+    private AtomicLong sequenceId = new AtomicLong(0);
+    private ConcurrentMap<String, Schema> schemaCache = new ConcurrentHashMap<String, Schema>();
+    public void sendSyncTask(Callable task) {
         if (channel == null) {
             logger.error("channel in null");
         }
